758e719ab3d384be735b31a4a5d143acca7f4d36
kitapawel <kitapawel@gmail.com>
2020-01-20 21:45:22 +0100

    ScheduleGeneratorTest - started writing tests for ParametersReader class.

--- src/main/java/schedule/parameters/ParametersReader.java
+++ src/main/java/schedule/parameters/ParametersReader.java
@@ -17,4 +17,5 @@ public class ParametersReader {
 
         return null;
     }
+
 }
---
+++ src/test/java/schedule/parameters/ParametersReaderTest.java
@@ -0,0 +1,44 @@
+package schedule.parameters;
+
+import org.junit.jupiter.api.Test;
+import schedule.generator.IScheduleGenerator;
+import schedule.generator.Lesson;
+import schedule.generator.Schedule;
+import schedule.generator.ScheduleGenerator;
+import schedule.holidays.CalendarificHolidayChecker;
+import schedule.holidays.DefaultHolidayChecker;
+import schedule.holidays.HolidayChecker;
+import schedule.parameters.EnteredParameters;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ParametersReaderTest {
+    @Test
+    void readParameters_AbleToParseEmptyParameterForRequiredHours() {
+        //given
+        LocalTime beginTime = LocalTime.of(10, 0);
+        LocalTime endTime = LocalTime.of(12, 0);
+        int requiredHours = 0;
+        LocalDate startDate = LocalDate.of(2020, 1, 1);
+        EnteredParameters expectedEnteredParameters = new EnteredParameters.Builder(beginTime, endTime, requiredHours)
+                .withLessonDays(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY))
+                .withStartDate(startDate)
+                .build();
+
+        //when
+        ParametersReader parametersReader = new ParametersReader();
+        int testedRequiredHours = parametersReader.readParameters(new String[]{});
+
+        //then
+        assertThat(expectedEnteredParameters.getRequiredHours(), equalTo(testedRequiredHours);
+    }
+}
